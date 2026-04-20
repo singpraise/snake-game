b3682595030046e4c757ee2f94e66b8c18bae60e
J Wan <tw.joycewan@gmail.com>
2026-04-20 19:50:44 +0800

    add: snake flask web game

---
+++ static/snake.js
@@ -0,0 +1,5 @@
+document.getElementById("score").textContent = "0";
+const cv = document.getElementById("cv");
+const ctx = cv.getContext("2d");
+ctx.fillStyle = "#4f4";
+ctx.fillRect(20, 20, 20, 20);
